232befbea3fbedf5e91d0039168f50c4f5835531
Rob Spectre <rob@brooklynhacker.com>
2024-11-08 17:47:51 -0500

    1) Setup project. 2) Created `PokemonCard` component. 3) Configured `Reveal` 4) Added four Pokemon cards.

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template lang='pug'>
+Reveal
+  .slides
+    PokemonCard(src='/images/putterpoop.png' background='fde16b')
+      .title.text-outline Grit
+      p.copy.text-white
+       | More copy
+    PokemonCard(src='/images/schmerr_card.png' background='ffe557')
+      .title.text-outline Snazzisms
+      p.copy.text-white
+       | This is an example of some text.
+    PokemonCard(src='/images/steam_roller_card.png')
+      .title.text-outline Rolled 
+      p.copy.text-white
+       | This is
+       | A bunch of stuff
+       | no linebreaks.
+    PokemonCard(src='/images/snoozica.png' background='232323')
+      .title.text-outline Snooze Abuse
+      p.copy.text-white
+       | More stuff
+</template>
+
+<script>
+import Reveal from '@/components/base/Reveal.vue'
+import Slide from '@/components/base/Slide.vue'
+import VideoSlide from '@/components/base/VideoSlide.vue'
+
+import PokemonCard from '@/components/PokemonCard.vue'
+
+export default {
+  name: 'JessBerfday',
+  components: {
+    Reveal,
+    Slide,
+    VideoSlide,
+    PokemonCard
+  }
+}
+</script>
+
+<style lang='scss'>
+@import url('https://fonts.cdnfonts.com/css/pokemon-solid');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap');
+
+.text-outline {
+  text-shadow:
+    8px 8px 0 #3c61a3,
+    -8px 8px 0 #3c61a3,
+    8px -8px 0 #3c61a3,
+    -8px -8px 0 #3c61a3;
+}
+
+.title {
+    font-family: 'Pokemon Solid', sans-serif;
+    @apply text-7xl pt-12 mx-auto uppercase text-pokemonYellow;
+}
+
+.copy {
+  font-family: 'Noto Sans', sans-serif;
+  @apply text-2xl px-4 py-4 text-left;
+}
+</style>
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,43 @@
+<template lang='pug'>
+VideoSlide(src='/video/background.mp4') 
+  .flex.place-content-center
+    VueFlip(
+      active-click
+      width='383px'
+      height='659px'
+    )
+      template(v-slot:front)
+        .w-full.h-full.flex.place-content-center
+          img.object-scale-down(:src='src')
+      template(v-slot:back)
+        .w-full.px-5(
+          class="h-4/5 mt-[44px] rounded-[20px]" 
+          :style="{ backgroundColor: `#${background}` }"
+        )
+          slot
+</template>
+
+<script>
+import { VueFlip } from 'vue-flip'
+
+import Reveal from '@/components/base/Reveal.vue'
+import VideoSlide from '@/components/base/VideoSlide.vue'
+
+export default {
+  name: 'PokemonCard',
+  components: {
+    VueFlip,
+    VideoSlide
+  },
+  props: {
+    src: String,
+    background: { 
+      type: String,
+      default: '95908a'
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
