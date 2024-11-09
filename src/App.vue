fc5f974e600965dbd0ce6b7a6c457bb147d8e00f
Rob Spectre <rob@brooklynhacker.com>
2024-11-09 11:53:06 -0500

    1) Added cards for Ada, Capacious Handpurse, Popcorn, Snoozica, Visor    and Two of Cups. 2) Fixed series counts. 3) Ordered deck.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,44 @@
 <template lang='pug'>
 Reveal
   .slides
-    PokemonCard(src='/images/putterpoop.png' background='fde16b')
-      .title.text-outline Grit
-      p.copy.text-white
-       | More copy
     PokemonCard(src='/images/schmerr_card.png' background='ffe557')
       .title.text-outline Snazzisms
       p.copy.text-white
        | This is an example of some text.
+    PokemonCard(src='/images/capacious_handpurse.png' background='eae1e0')
+      .title.text-outline Ready to Go
+      p.copy.text-white
+       | Stuff
+    PokemonCard(src='/images/putterpoop.png' background='fde16b')
+      .title.text-outline Grit
+      p.copy.text-white
+       | More copy
+    PokemonCard(src='/images/popcorn.png' background='da3789')
+      .title.text-outline Her Taste
+      p.copy.text-white
+       | Copy
     PokemonCard(src='/images/steam_roller_card.png')
       .title.text-outline Rolled 
       p.copy.text-white
        | This is
        | A bunch of stuff
        | no linebreaks.
+    PokemonCard(src='/images/visor.png' background='305c75')
+      .title.text-outline E-Town
+      p.copy.text-white
+       | Text
     PokemonCard(src='/images/snoozica.png' background='232323')
       .title.text-outline Snooze Abuse
       p.copy.text-white
        | More stuff
+    PokemonCard(src='/images/ada.png' background='084182')
+      .title.text-outline The Meeting
+      p.copy.text-white
+       | Copy
+    PokemonCard(src='/images/two_of_cups.png' background='393738')
+      .title.text-outline First Kiss
+      p.copy.text-white
+       | Text
 </template>
 
 <script>
